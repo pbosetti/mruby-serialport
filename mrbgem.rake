d9827640f7cdf52be8586219ab6800ee1c98d4fa
Paolo Bosetti <paolo.bosetti@unitn.it>
2015-02-27 18:00:56 +0100

    Added support for travis-ci

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,4 +1,8 @@
 MRuby::Gem::Specification.new('mruby-serialport') do |spec|
-  spec.license = 'MIT'
   spec.author  = 'Paolo Bosetti 2013'
+  spec.license = 'GPL 2.0'
+  spec.summary = 'FTP client library'
+  spec.version = 0.1
+  spec.description = spec.summary
+  spec.homepage = "http://github.com/pbosetti/mruby-serialport"
 end
--- src/serialport.c
+++ src/serialport.c
@@ -4,12 +4,10 @@
 ** See Copyright Notice in mruby.h
 */
 
-
 #include "mruby.h"
 #include "mruby/class.h"
 #include "mruby/string.h"
 #include "mruby/variable.h"
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,79 +18,70 @@
 #include <fcntl.h>
 
 #define IV_GET(name) mrb_iv_get(mrb, self, mrb_intern_cstr(mrb, (name)))
-#define IV_SET(name, value) mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, (name)), value)
+#define IV_SET(name, value)                                                    \
+  mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, (name)), value)
 
-enum error_src {
-  TCGETADDR_ERROR = 1,
-  TCSETADDR_ERROR
-};
+enum error_src { TCGETADDR_ERROR = 1, TCSETADDR_ERROR };
 
-
-enum error_src
-set_interface_attribs (int fd, int speed, int parity)
-{
+enum error_src set_interface_attribs(int fd, int speed, int parity) {
   struct termios tty;
-  memset (&tty, 0, sizeof tty);
-  if (tcgetattr (fd, &tty) != 0)
+  memset(&tty, 0, sizeof tty);
+  if (tcgetattr(fd, &tty) != 0)
     return TCGETADDR_ERROR;
 
-  cfsetospeed (&tty, speed);
-  cfsetispeed (&tty, speed);
+  cfsetospeed(&tty, speed);
+  cfsetispeed(&tty, speed);
 
-  tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+  tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
   // disable IGNBRK for mismatched speed tests; otherwise receive break
   // as \000 chars
-  tty.c_iflag &= ~IGNBRK;   // ignore break signal
-  tty.c_lflag = 0;    // no signaling chars, no echo,
-          // no canonical processing
-  tty.c_oflag = 0;    // no remapping, no delays
-  tty.c_cc[VMIN]  = 0;      // read doesn't block
-  tty.c_cc[VTIME] = 5;      // 0.5 seconds read timeout
+  tty.c_iflag &= ~IGNBRK; // ignore break signal
+  tty.c_lflag = 0;        // no signaling chars, no echo,
+                          // no canonical processing
+  tty.c_oflag = 0;        // no remapping, no delays
+  tty.c_cc[VMIN] = 0;     // read doesn't block
+  tty.c_cc[VTIME] = 5;    // 0.5 seconds read timeout
 
   tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
 
-  tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-          // enable reading
-  tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+  tty.c_cflag |= (CLOCAL | CREAD);   // ignore modem controls,
+                                     // enable reading
+  tty.c_cflag &= ~(PARENB | PARODD); // shut off parity
   tty.c_cflag |= parity;
   tty.c_cflag &= ~CSTOPB;
   tty.c_cflag &= ~CRTSCTS;
 
-  if (tcsetattr (fd, TCSANOW, &tty) != 0)
+  if (tcsetattr(fd, TCSANOW, &tty) != 0)
     return TCSETADDR_ERROR;
   else
     return 0;
 }
 
-enum error_src
-set_blocking (int fd, int should_block)
-{
+enum error_src set_blocking(int fd, int should_block) {
   struct termios tty;
-  memset (&tty, 0, sizeof tty);
-  if (tcgetattr (fd, &tty) != 0)
+  memset(&tty, 0, sizeof tty);
+  if (tcgetattr(fd, &tty) != 0)
     return TCGETADDR_ERROR;
 
-  tty.c_cc[VMIN]  = should_block ? 1 : 0;
-  tty.c_cc[VTIME] = 5;      // 0.5 seconds read timeout
+  tty.c_cc[VMIN] = should_block ? 1 : 0;
+  tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
 
-  if (tcsetattr (fd, TCSANOW, &tty) != 0)
+  if (tcsetattr(fd, TCSANOW, &tty) != 0)
     return TCSETADDR_ERROR;
   else
     return 0;
 }
 
-mrb_value
-mrb_serialport_open(mrb_state *mrb, mrb_value self)
-{
+mrb_value mrb_serialport_open(mrb_state *mrb, mrb_value self) {
   int fd;
   mrb_value mrb_portname = IV_GET("@port_name");
-  mrb_value mrb_baud     = IV_GET("@baud");
+  mrb_value mrb_baud = IV_GET("@baud");
   mrb_value mrb_blocking = IV_GET("@blocking");
-  
-  char *portname         = mrb_string_value_cstr(mrb, &mrb_portname);
-  unsigned int baud      = mrb_fixnum(mrb_baud);
-  
-  fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+
+  const char *portname = mrb_string_value_cstr(mrb, &mrb_portname);
+  unsigned int baud = mrb_fixnum(mrb_baud);
+
+  fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
   if (fd < 0) {
     IV_SET("@error", mrb_fixnum_value(errno));
     mrb_raise(mrb, E_RUNTIME_ERROR, "Could not open port");
@@ -113,96 +102,82 @@ mrb_serialport_open(mrb_state *mrb, mrb_value self)
   return self;
 }
 
-mrb_value
-mrb_serialport_close(mrb_state *mrb, mrb_value self)
-{
+mrb_value mrb_serialport_close(mrb_state *mrb, mrb_value self) {
   int fd = mrb_fixnum(IV_GET("@fd"));
   if (fd >= 0) {
     close(fd);
     IV_SET("@fd", mrb_fixnum_value(-1));
     return mrb_true_value();
-  }
-  else {
+  } else {
     return mrb_false_value();
   }
 }
 
-mrb_value
-mrb_serialport_write(mrb_state *mrb, mrb_value self)
-{
+mrb_value mrb_serialport_write(mrb_state *mrb, mrb_value self) {
   mrb_value r_string;
   char *string;
   size_t l = 0;
   int fd = mrb_fixnum(IV_GET("@fd"));
-  
+
   if (fd >= 0) {
     mrb_get_args(mrb, "S", &r_string);
     string = mrb_str_to_cstr(mrb, r_string);
-    l      = write(fd, string, strlen(string));
+    l = write(fd, string, strlen(string));
     return mrb_fixnum_value(l);
-  }
-  else {
+  } else {
     return mrb_nil_value();
   }
 }
 
-mrb_value
-mrb_serialport_p_read(mrb_state *mrb, mrb_value self)
-{
+mrb_value mrb_serialport_p_read(mrb_state *mrb, mrb_value self) {
   mrb_value r_buffer_size, r_result;
   size_t buf_len;
   char *string = NULL;
   int fd = mrb_fixnum(IV_GET("@fd"));
-  
+
   if (fd >= 0) {
     mrb_get_args(mrb, "i", &r_buffer_size);
     buf_len = mrb_fixnum(r_buffer_size);
-    string  = (char *)realloc(string, buf_len * sizeof(char));
+    string = (char *)realloc(string, buf_len * sizeof(char));
     if (!string) {
       free(string);
       string = NULL;
       mrb_raise(mrb, E_RUNTIME_ERROR, "Could not allocate memory");
     }
     bzero(string, buf_len);
-    buf_len  = read(fd, string, buf_len * sizeof(char));
+    buf_len = read(fd, string, buf_len * sizeof(char));
     r_result = mrb_str_new_cstr(mrb, string);
     free(string);
     return r_result;
-  }
-  else {
+  } else {
     return mrb_nil_value();
   }
 }
 
-mrb_value
-mrb_serialport_read_char(mrb_state *mrb, mrb_value self)
-{
+mrb_value mrb_serialport_read_char(mrb_state *mrb, mrb_value self) {
   char ch[1];
   int fd = mrb_fixnum(IV_GET("@fd"));
   if (fd >= 0) {
     read(fd, ch, sizeof(char));
     return mrb_str_new_cstr(mrb, ch);
-  }
-  else {
+  } else {
     return mrb_nil_value();
   }
 }
 
-
-
-void
-mrb_mruby_serialport_gem_init(mrb_state* mrb)
-{
+void mrb_mruby_serialport_gem_init(mrb_state *mrb) {
   struct RClass *serialport_class;
   serialport_class = mrb_define_class(mrb, "SerialPort", mrb->object_class);
-  mrb_define_method(mrb, serialport_class, "open", mrb_serialport_open, MRB_ARGS_NONE());
-  mrb_define_method(mrb, serialport_class, "close", mrb_serialport_close, MRB_ARGS_NONE());
-  mrb_define_method(mrb, serialport_class, "write", mrb_serialport_write, MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, serialport_class, "_read", mrb_serialport_p_read, MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, serialport_class, "read_char", mrb_serialport_read_char, MRB_ARGS_NONE());
+  mrb_define_method(mrb, serialport_class, "open", mrb_serialport_open,
+                    MRB_ARGS_NONE());
+  mrb_define_method(mrb, serialport_class, "close", mrb_serialport_close,
+                    MRB_ARGS_NONE());
+  mrb_define_method(mrb, serialport_class, "write", mrb_serialport_write,
+                    MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, serialport_class, "_read", mrb_serialport_p_read,
+                    MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, serialport_class, "read_char",
+                    mrb_serialport_read_char, MRB_ARGS_NONE());
 }
 
-void
-mrb_mruby_serialport_gem_final(mrb_state* mrb)
-{
-}
+void mrb_mruby_serialport_gem_final(mrb_state *mrb) {}
